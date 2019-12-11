abdd4f780e588982485b9547d6b9a655de2d215b
Karthik Chalamalasetti <54052517+karthikch25@users.noreply.github.com>
2019-12-12 05:23:46 +0530

    Create doubly_linked_list.c

---
+++ doubly_linked_list.c
@@ -0,0 +1,82 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node{
+    int data;
+    struct node *prev;
+    struct node *next;
+}linkedlistnode;
+linkedlistnode *head=NULL;
+void insertnode(int data,int position)
+{
+    linkedlistnode * t=head;
+    t->prev=NULL;
+    linkedlistnode * newnode=(linkedlistnode*)malloc(sizeof(linkedlistnode));
+    newnode->data=data;
+    if(position==1)
+    {  newnode->next=head;
+        head=newnode->prev;
+        return;
+    }
+        for(int i=1;i<=position-1;i++)
+        {
+            t=t->next;
+            
+        }
+         newnode->next=t->next;
+         newnode->prev=t;
+         t->next=newnode;
+         t->next->prev=newnode;
+                
+}
+/*void delete(int position)
+{linkedlistnode* t=head;
+    if(position==1)
+    {
+        t->next->prev=head;
+        
+        return;
+    }
+   for(int i=1;i<position-1;i++)
+        {
+            t=t->next;
+        }
+    t->next=t->next->next;
+}*/
+void print()
+{linkedlistnode *t=head;
+    while(t!=NULL)
+    {
+        printf("%d->",t->data);
+        t=t->next;
+    }printf("NULL\n");
+}
+int main()
+{   int val,pos,n;
+    linkedlistnode* head=NULL;
+    linkedlistnode* temp;
+    while(1)
+    {
+        printf("1.insert at nth position\n");
+        printf("2.delete at n position\n");
+        printf("enter your choice:");
+        scanf("%d",&n);
+    switch(n)
+    {
+     case 1: printf("enter data:");
+            scanf("%d",&val);
+            printf("enter position:");
+            scanf("%d",&pos);
+            insertnode(val,pos);
+            print();
+            break;
+    /*case 2: printf("enter the position of the deleting node:");
+            scanf("%d",&pos);
+            delete(pos);
+            print();
+            break;*/
+    default: printf("enter valid number");
+            break;
+    }
+    }
+}
